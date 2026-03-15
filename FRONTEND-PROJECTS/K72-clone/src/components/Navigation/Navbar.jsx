344dbf86165e2d5ddd834d26edcb35d3336acc59
Harshvardhan Puranik <hvp0926@gmail.com>
2026-03-15 17:19:50 +0530

    navbar

--- FRONTEND-PROJECTS/K72-clone/src/components/Navigation/Navbar.jsx
+++ FRONTEND-PROJECTS/K72-clone/src/components/Navigation/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div>
         <div className="h-1/10 w-screen z-10 fixed text-white flex justify-between">
-            <div className='p-2 w-[150px] h-[150px]'><img className='h-100% w-100% bg-cover' src="src\assets\output-onlinepngtools.png" alt="" /></div>
+            <div className='p-2 w-37.5 h-37.5'><img className='h-100% w-100% bg-cover' src="src\assets\output-onlinepngtools.png" alt="" /></div>
             <div className=''><RiMenuLine /></div>
         </div>
     </div>
