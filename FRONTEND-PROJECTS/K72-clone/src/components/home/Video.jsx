1fd1fdffab0dfe827b591aa4f84e54f313013252
Harshvardhan Puranik <hvp0926@gmail.com>
2026-03-19 13:15:51 +0530

    video

--- FRONTEND-PROJECTS/K72-clone/src/components/home/Video.jsx
+++ FRONTEND-PROJECTS/K72-clone/src/components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' src="src\assets\69496b2d.mp4" muted loop autoPlay></video>
+        <video className='h-full w-full object-cover' src="https://download-video-ak.vimeocdn.com/v3-1/playback/36bc59b8-6671-4358-abc2-15555fc6ae59/69496b2d?__token__=st=1773906314~exp=1773909914~acl=%2Fv3-1%2Fplayback%2F36bc59b8-6671-4358-abc2-15555fc6ae59%2F69496b2d%2A~hmac=1688f210667836f0801140820b7724b12eca8b4b7065e0fca870e1a54cdb48a4&r=dXMtZWFzdDE%3D" muted loop autoPlay></video>
     </div>
   )
 }
