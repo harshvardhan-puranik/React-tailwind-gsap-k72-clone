e2b822f065eece43afcfffd09cff3ee9fb0acfdf
Harshvardhan Puranik <hvp0926@gmail.com>
2026-03-19 15:16:14 +0530

    video

--- FRONTEND-PROJECTS/K72-clone/src/components/home/Video.jsx
+++ FRONTEND-PROJECTS/K72-clone/src/components/home/Video.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Video = () => {
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' src="\src\assets\69496b2d.mp4" muted loop autoPlay></video>
+        <video className='h-full w-full object-cover' src="https://res.cloudinary.com/dbft8cejv/video/upload/v1773913442/69496b2d_kichqu.mp4" muted loop autoPlay></video>
     </div>
   )
 }
